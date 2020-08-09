c67a318e6d2f284a64d35cfb1938c33901b02d92
Yuanzhe Zhou <yuanzhezhou@gmail.com>
2020-08-09 11:57:21 -0500

    First commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+repo_path=~/repo/configs/
+ln -s ${repo_path}/zshrc ~/.zshrc
+ln -s ${repo_path}/ssh_config ~/.ssh/config
+ln -s ${repo_path}/vimrc ~/.vimrc
+
+# if *BSD
+chmod -h 644 ~/.zshrc
+chmod -h 644 ~/.ssh/config
+chmod -h 644 ~/.vimrc
